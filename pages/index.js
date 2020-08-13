8e5341a74fa8d3e57374e7dba81077d21d63595a
Joseph Surla <josephsurla@gmail.com>
2020-08-13 15:23:23 +0900

    Change font family to Inter

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,29 @@ export default function IndexPage() {
   return (
     <div>
       <Nav />
-      <div className="py-20">
+      <div className="container mx-auto py-20">
         <h1 className="text-3xl text-center text-accent-1"></h1>
-        <div>
-          <p className="text-2xl">
-            👋Hi, I'm Joseph Surla and I enjoy building things with code.
-            Currently living in beautiful Japan. 🇯🇵
+        <div className="">
+          <p className="text-5xl font-bold">👋Hi, I'm Joseph Surla</p>
+          <p className="text-3xl">
+            I enjoy building things with code. Currently living in beautiful
+            Japan. 🇯🇵
           </p>
         </div>
       </div>
+
+      {/*social media links*/}
+      <ul>
+        <li>
+          <a href="https://github.com/surla">Github</a>
+        </li>
+        <li>
+          <a href="https://twitter.com/josephsurla">Twitter</a>
+        </li>
+        <li>
+          <a href="https://codesandbox.io/u/surla/">CodeSandbox</a>
+        </li>
+      </ul>
     </div>
   );
 }
