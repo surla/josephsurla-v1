ac28c029bfb39486ea35af356ffc5fef1806f19f
Joseph Surla <josephsurla@gmail.com>
2020-08-14 12:02:47 +0900

    Create contact component

---
+++ components/contact.js
@@ -0,0 +1,19 @@
+export default function Contact() {
+  return (
+    <div className="container mx-auto w-10/12 mt-4 bg-pink-100 rounded-md p-4">
+      <div className="text-gray-900 leading-3 tracking-wide text-lg">
+        Let talk!
+      </div>
+      &mdash;
+      <p className="text-gray-800">
+        I am currently seeking new job opportunities in Japan.
+      </p>
+      <p className="text-gray-800">
+        You can reach me at:{" "}
+        <span className="underline hover:text-blue-500 text-gray-900 text-2xl">
+          hello@josephsurla.com
+        </span>
+      </p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,28 @@
 import Nav from "../components/nav";
 import Projects from "../components/projects";
+import Social from "../components/social";
+import Footer from "../components/footer";
+import Contact from "../components/contact";
 
 export default function IndexPage() {
   return (
-    <div>
-      <Nav />
+    <div className="bg-gray-100">
       <div className="container mx-auto w-4/5 py-20">
         <div className="text-left">
-          <p className=" text-gray-800 sm:text-lg lg:text-4xl">
+          <p className=" text-gray-800 font-bold tracking-wide sm:text-xl md:text-3xl lg:text-5xl">
             Hello, I'm Joseph Surla.
           </p>
-          <p className="text-gray-600 sm:text-lg lg:text-lg ">
-            A software developer who currently lives in Japan. 🇯🇵
+          <p className="text-gray-700 sm:text-xl lg:text-2xl">
+            A software developer who lives in Japan. 🇯🇵
           </p>
+          <p className="text-gray-600">I love building things with code.</p>
         </div>
       </div>
 
       <Projects />
+      <Social />
+      <Contact />
+      <Footer />
 
       {/*social media links*/}
       {/* <ul>
