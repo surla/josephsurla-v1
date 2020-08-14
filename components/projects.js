61963329586401ef0cf5fe5491730b0bb8ebbf94
Joseph Surla <josephsurla@gmail.com>
2020-08-14 17:07:33 +0900

    Add flexbox to projects

--- components/projects.js
+++ components/projects.js
@@ -1,33 +1,47 @@
 const projects = [
   {
     title: "Codelabs",
+    url: "https://github.com/surla/",
     description:
       "A collection of small projects. The focus of this project is to create as many coding projects.",
-    technology: "Built with Nextjs and styled with Tailwind CSS",
+    tags: ["Javascript", "Next.js", "Tailwind CSS"],
   },
   {
     title: "Quote List",
+    url: "https://github.com/surla/quote-list",
     description: "Web application that creates and saves your favorite quotes.",
-    technology: "Built with Sinatra",
+    tags: ["Ruby", "Sinatra"],
+  },
+  {
+    title: "Class Roster App",
+    url: "https://github.com/surla/ClassRosterApp",
+    description: "CLI application that parsed student data.",
+    tags: ["C++"],
   },
 ];
 
 export default function Projects() {
   return (
     <div className="container mx-auto bg-green-100 w-10/12 rounded-md p-4">
-      <h3 className="text-gray-700 leading-3 tracking-wide">
+      <h3 className="text-xs lg:text-base text-gray-700 leading-3 tracking-wide">
         Things I've worked on
       </h3>
       &mdash;
-      <div className="">
-        {projects.map(({ title, description, technology }) => (
-          <div>
-            <div className="text-2xl font-bold text-gray-900 tracking-wide">
-              {title}
+      <div className="flex flex-col content-between text-xs lg:text-base">
+        {projects.map(({ title, url, description, tags }) => (
+          <a href={url}>
+            <div className="p-4 m-1 border-solid border-2 border-gray-300 rounded-md hover:border-blue-300 hover:bg-blue-200">
+              <div className="text-lg lg:text-2xl font-bold text-gray-900 tracking-wide">
+                {title}
+              </div>
+              <p className="text-gray-800">{description}</p>
+              {tags.map((value) => (
+                <span className="text-xs pl-2 pr-2 mr-2 bg-orange-200 rounded-md text-black">
+                  {value}
+                </span>
+              ))}
             </div>
-            <p className="text-gray-800">{description}</p>
-            <p className="text-md text-gray-500">{technology}</p>
-          </div>
+          </a>
         ))}
       </div>
     </div>
