e751abcb51432ed8a4ab2856cf84db91964bf579
Joseph Surla <josephsurla@gmail.com>
2020-08-14 11:16:52 +0900

    Create footer component

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="container mx-auto w-10/12 mt-8 pt-2 pb-2 border-t border-gray-500">
+      <div className="text-xs text-gray-600">
+        &copy; By Joseph Surla. Built with Next.js and styled with Tailwind CSS.
+      </div>
+    </div>
+  );
+}
